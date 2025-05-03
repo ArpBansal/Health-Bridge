22f7440d5be768705272b529bf43c3ba66276135
Ayush Rai <raiayush3366@gmail.com>
2025-05-03 16:34:54 +0530

    render asgi config

--- Backend/HeatlhBridgeBackend/asgi.py
+++ Backend/HeatlhBridgeBackend/asgi.py
@@ -1,10 +1,13 @@
 import os
-
+import django
 from django.core.asgi import get_asgi_application
-from chat.websocket.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HeatlhBridgeBackend.settings')
+django.setup()
+
+from chat.websocket.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
